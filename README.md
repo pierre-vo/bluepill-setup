9eabdd0344bf592526dd0de1e97fafcc98367679
Pierre V <746601+pierre-vo@users.noreply.github.com>
2022-08-21 16:49:22 +0200

    Cleanup
    * feat(clang): clang-format + clang-tidy
    * chore: cleanup
    * fix: relax constraints (remove -Werror, remove clang-diagnostic-*)
    * chore(cformat): main.c

--- README.md
+++ README.md
@@ -16,33 +16,12 @@ Set up code for bluepill HW
 
 ### Rust
 
+## Compile
+
+`cmake -Bbuild -G "Unix Makefiles" --toolchain arm-toolchain.cmake`
 
 ## Temporary notes
 
 
-mkdir build
-cd build
-cmake ../ -G "MinGW Makefiles"
-cmake ../ -G "MSYS Makefiles"
-cmake --build .
-
-
-
-cmake ../ -D"CMAKE_MAKE_PROGRAM:PATH=C:/App/msys64/usr/bin/make.exe" -G "MSYS Makefiles"
-
-
-```
-cmake_minimum_required(VERSION 3.0.0)
-project(MainTest VERSION 0.1.0)
-set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-# set(CMAKE_C_CLANG_TIDY clang-tidy -checks=-*,clang-diagnostic-*,clang-analyzer-*,readability-*)
-add_compile_options(-Wall -Wextra -pedantic -Werror)
-add_executable(MainTest src/main.c)
-```
-
-`cmake -B_builds -G "Ninja Multi-Config" --toolchain /path/to/xxx-toolchain.cmake`
-
-`cmake -B_builds -G "Ninja Multi-Config" --toolchain xxx-toolchain.cmake`
-
-
-https://github.com/montoyo/bluepill-base
+## Links
+- https://github.com/montoyo/bluepill-base
--- src/main.c
+++ src/main.c
@@ -1,81 +1,89 @@
 #include "stm32f10x.h"
-#include "system_stm32f10x.h"
+// #include "system_stm32f10x.h"
 #include "core_cm3.h"
 #include <stdbool.h>
+#include <stdint.h>
 
 static void dummySleep(uint32_t cnt)
 {
-    uint32_t i;
-    for(i = 0; i < cnt; i++)
-        __NOP();
+  uint32_t icnt;
+  for (icnt = 0; icnt < cnt; icnt++)
+  {
+    __NOP();
+  }
 }
 
 static void setupUSART()
 {
-    // Tx on PA.9
-    RCC->APB2ENR  |=  RCC_APB2ENR_IOPAEN;   //Enable port A clock
-    RCC->APB2RSTR |=  RCC_APB2RSTR_IOPARST; //Reset port A
-    RCC->APB2RSTR &= ~RCC_APB2RSTR_IOPARST;
+  // Tx on PA.9
+  RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;    // Enable port A clock
+  RCC->APB2RSTR |= RCC_APB2RSTR_IOPARST; // Reset port A
+  RCC->APB2RSTR &= ~RCC_APB2RSTR_IOPARST;
 
-    RCC->APB2ENR  |=  RCC_APB2ENR_USART1EN;   //Enable USART1 clock
-    RCC->APB2RSTR |=  RCC_APB2RSTR_USART1RST; //Reset USART1
-    RCC->APB2RSTR &= ~RCC_APB2RSTR_USART1RST;
+  RCC->APB2ENR |= RCC_APB2ENR_USART1EN;    // Enable USART1 clock
+  RCC->APB2RSTR |= RCC_APB2RSTR_USART1RST; // Reset USART1
+  RCC->APB2RSTR &= ~RCC_APB2RSTR_USART1RST;
 
-    //Configure USART
-    USART1->CR1 &= ~USART_CR1_UE;
-    USART1->CR1  = USART_CR1_TE | USART_CR1_RE;
-    USART1->CR2  = 0;
-    USART1->CR3  = 0;
-    USART1->BRR  = 0x271; //115200 bauds for 72 MHz
-    USART1->GTPR = 1;
+  // Configure USART
+  USART1->CR1 &= ~USART_CR1_UE;
+  USART1->CR1 = USART_CR1_TE | USART_CR1_RE;
+  USART1->CR2 = 0;
+  USART1->CR3 = 0;
+  USART1->BRR = 0x271; // 115200 bauds for 72 MHz
+  USART1->GTPR = 1;
 
-    //Configure pins & enable USART
-    GPIOA->CRH = (GPIOA->CRH & ~(GPIO_CRH_CNF9 | GPIO_CRH_MODE9)) | GPIO_CRH_CNF9_1 | GPIO_CRH_MODE9_1 | GPIO_CRH_MODE9_0;
-    USART1->CR1 |= USART_CR1_UE;
+  // Configure pins & enable USART
+  GPIOA->CRH = (GPIOA->CRH & ~(GPIO_CRH_CNF9 | GPIO_CRH_MODE9)) |
+               GPIO_CRH_CNF9_1 | GPIO_CRH_MODE9_1 | GPIO_CRH_MODE9_0;
+  USART1->CR1 |= USART_CR1_UE;
 }
 
 static void writeUSART(const char *str)
 {
-    while(*str != 0) {
-        while((USART1->SR & USART_SR_TXE) == 0);
-        USART1->DR = (uint8_t) *str;
-        str++;
-    }
+  while (*str != 0)
+  {
+    while ((USART1->SR & USART_SR_TXE) == 0)
+    {
+    };
+    USART1->DR = (uint8_t)*str;
+    str++;
+  }
 }
 
 static void setupLED_B()
 {
-    RCC->APB2ENR  |= RCC_APB2ENR_IOPBEN;   //Enable port B clock
-    RCC->APB2RSTR |= RCC_APB2RSTR_IOPBRST; //Reset port B
-    RCC->APB2RSTR &= ~RCC_APB2RSTR_IOPBRST;
+  RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;    // Enable port B clock
+  RCC->APB2RSTR |= RCC_APB2RSTR_IOPBRST; // Reset port B
+  RCC->APB2RSTR &= ~RCC_APB2RSTR_IOPBRST;
 
-    GPIOB->CRL = (GPIOB->CRL & ~GPIO_CRL_CNF2) | GPIO_CRL_MODE2_1; //Output 2 MHz
+  GPIOB->CRL = (GPIOB->CRL & ~GPIO_CRL_CNF2) | GPIO_CRL_MODE2_1; // Output 2 MHz
 }
 
 static void setupLED_C()
 {
-    RCC->APB2ENR  |= RCC_APB2ENR_IOPCEN;   //Enable port C clock
-    RCC->APB2RSTR |= RCC_APB2RSTR_IOPCRST; //Reset port C
-    RCC->APB2RSTR &= ~RCC_APB2RSTR_IOPCRST;
+  RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;    // Enable port C clock
+  RCC->APB2RSTR |= RCC_APB2RSTR_IOPCRST; // Reset port C
+  RCC->APB2RSTR &= ~RCC_APB2RSTR_IOPCRST;
 
-    GPIOC->CRH = (GPIOC->CRH & ~GPIO_CRH_CNF13) | GPIO_CRH_MODE13_1; //Output 2 MHz
+  GPIOC->CRH =
+      (GPIOC->CRH & ~GPIO_CRH_CNF13) | GPIO_CRH_MODE13_1; // Output 2 MHz
 }
 
 void main()
 {
-    //Called by startup.s
-    SystemInit(); //Initialize clock to 72 MHz
-    setupUSART(); //Initialize USART to 115200 bauds
-    setupLED_B();
-    setupLED_C();
+  // Called by startup.s
+  SystemInit(); // Initialize clock to 72 MHz
+  setupUSART(); // Initialize USART to 115200 bauds
+  setupLED_B();
+  setupLED_C();
 
-    while(true) {
-        //Blink
-        GPIOB->ODR ^= GPIO_ODR_ODR2; // B2, not working?
-        GPIOC->ODR ^= GPIO_ODR_ODR13; // C13
-        writeUSART("It works!\r\n");
-        ITM_SendChar('a');
-        dummySleep(500000U);
-    }
+  while (true)
+  {
+    // Blink
+    GPIOB->ODR ^= GPIO_ODR_ODR2;  // B2, not working?
+    GPIOC->ODR ^= GPIO_ODR_ODR13; // C13
+    writeUSART("It works!\r\n");
+    ITM_SendChar('a');
+    dummySleep(500000U);
+  }
 }
-
